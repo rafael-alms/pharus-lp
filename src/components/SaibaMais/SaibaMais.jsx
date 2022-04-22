52e92ab72b9834c2999d468d488c6483c2da5798
Rafael de Almeida <rafaelde-almeida@hotmail.com>
2022-04-22 18:46:38 -0300

    final updates

--- src/components/SaibaMais/SaibaMais.jsx
+++ src/components/SaibaMais/SaibaMais.jsx
@@ -13,7 +13,7 @@ const SaibaMais = (props) => {
                                 <p className='titulo'>Mais sobre o Pharus</p>
                                 <p className='conteudo'>O Pharus nasceu através da leitura e estudo da ODS 8 da ONU, como parte do desafio final da 4ª Edição do Camp da Ioasys.</p>
                                 <p className='conteudo paragrafo'>O time é composto por 11 integrantes de diversas áreas e localidades do país, através da aplicação de metodologia de Discovery e Produtos, focaram o trabalho para melhoria da capacitação de jovens do ensino médio ao fundamental para Escolas Públicas.</p>
-                                <p className='conteudo paragrafo'>Para maiores informações e detalhes sobre o processo de Discovery, <a href='https://ftomoda.notion.site/8-ODS-Emprego-Digno-e-Crescimento-Econ-mico-1105078741c240e5a9bcd994e72d0abf' target="_blank">clique aqui</a>.</p>
+                                <p className='conteudo paragrafo'>Para maiores informações e detalhes sobre o processo de Discovery, <a href='https://www.notion.so/Discovery-2d3ea690bc8042b9a1884cab24d777bf' target="_blank">clique aqui</a>.</p>
                             </div>
                         <img src={ods8} alt="ODS 8" />
                     </div>
